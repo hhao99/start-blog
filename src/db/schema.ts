0b81a309a4aebb30657b5744aaebb1d8ab0d60df
hhao99 <hhao99@gmail.com>
2026-08-03 23:52:15 -0500

    server only funciton

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -14,5 +14,25 @@ export const users = sqliteTable('users', {
   ),
 })
 
+
+export const articles = sqliteTable('articles', {
+  id: integer({ mode: 'number' }).primaryKey({
+    autoIncrement: true,
+  }),
+  title: text().notNull(),
+  content: text().notNull(),
+  authorId: integer('author_id').notNull().references(() => users.id),
+  createdAt: integer('created_at', { mode: 'timestamp' }).default(
+    sql`(unixepoch())`,
+  ),
+  updatedAt: integer('updated_at', { mode: 'timestamp' }).default(
+    sql`(unixepoch())`,
+  ),
+})
+
+
+
 export type User = typeof users.$inferSelect
 export type NewUser = typeof users.$inferInsert
+export type Article = typeof articles.$inferSelect
+export type NewArticle = typeof articles.$inferInsert
